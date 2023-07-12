8e3c91b0b4b84bfe1281d6db76105d8c866225d0
qnShen <shenqinan@cnzxa.cn>
2023-07-12 14:24:47 +0800

    优化

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 # @Time : 2023/7/9 23:15
 # @Author : shenbq
-
+import yaml
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -9,25 +9,15 @@ from utils import driver
 
 
 class Helpers():
-
-    def __init__(self, driver):
-        self.driver = driver
-
-    def wait_for_element(self, locator, timeout=10):
-        return WebDriverWait(self.driver, timeout).until(EC.presence_of_element_located(locator))
-
-    def click(self, locator):
-        self.wait_for_element(locator).click()
-
-    def input(self, locator, text):
-        self.wait_for_element(locator).clear()
-        self.wait_for_element(locator).send_keys(text)
-
-    # 使用示例
-
-
-# from locators import MainPageLocators
-#
-# helpers = Helpers(driver)
-# helpers.click(MainPageLocators.LOGIN_BTN)
-# helpers.input(MainPageLocators.SEARCH_BOX, "Automation")
+    @staticmethod
+    def read_yaml(yaml_file_path):
+        with open(yaml_file_path,'r') as f:
+            try:
+                return yaml.safe_load(f)
+            except yaml.YAMLError as e:
+                print(e)
+    # @staticmethod
+    # def write_yaml(yaml_file_path):
+    #     with open(yaml_file_path,'a') as f:
+    #         try:
+    #             return
